22d7b40b3ab37e86967039a78ca38a8e2a398e37
kenshinkirii <98351428+kenshinkirii@users.noreply.github.com>
2022-02-01 00:56:23 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    while (password != "img" && password != "tennis") {
+        music.playMelody("- - - - - - - - ", 120)
+    }
+})
 function succes (sprite: string) {
     scene.setBackgroundColor(1)
     return "Login succesfull " + "Hi, " + name
@@ -8,20 +13,22 @@ function failed (sprite: string) {
 }
 let fail = ""
 let success = ""
+let password = ""
 let name = ""
+scene.setBackgroundColor(8)
 game.splash("Welcome!")
 // asks what is your name
 name = game.askForString("What is your name?")
 game.splash("Hello " + name + "!")
 // asks for what your password is
-let password = game.askForString("What is your password?")
-timer.debounce(success, 2000, function () {
+password = game.askForString("What is your password?")
+timer.debounce(password, 2000, function () {
     game.splash("Times up")
     music.wawawawaa.play()
     game.over(false)
 })
 // input 1 and arcade equals together
-if (password == "img" || password == "tennis") {
+if (true) {
     success = succes("Login succesfull " + "Hi, " + name)
     game.splash(success)
 } else {
