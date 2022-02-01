6697763c538e42ec96e1474c06ab3e8baf678c3a
kenshinkirii <98351428+kenshinkirii@users.noreply.github.com>
2022-02-01 00:50:54 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,23 +8,25 @@ function failed (sprite: string) {
 }
 let fail = ""
 let success = ""
-let password = ""
 let name = ""
-scene.setBackgroundColor(13)
 game.splash("Welcome!")
 // asks what is your name
 name = game.askForString("What is your name?")
 game.splash("Hello " + name + "!")
-while (!(password == "ab" || password == "Leo")) {
-    music.wawawawaa.play()
-}
 // asks for what your password is
-password = game.askForString("What is your password?")
+let password = game.askForString("What is your password?")
+timer.debounce(success, 2000, function () {
+    game.splash("Times up")
+    music.wawawawaa.play()
+    game.over(false)
+})
 // input 1 and arcade equals together
-if (password == "ab" || password == "Leo") {
+if (password == "img" || password == "tennis") {
     success = succes("Login succesfull " + "Hi, " + name)
     game.splash(success)
 } else {
     fail = failed("Login failed " + "try again " + name)
     game.splash(fail)
+    // asks for what your password is
+    password = game.askForString("What is your password?")
 }
